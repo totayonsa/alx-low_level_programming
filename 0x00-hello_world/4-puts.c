6dec261c6bb42ebbec6d3ae195c4f483ce9f95c2
totayonsa <dankipesa@gmail.com>
2023-02-20 14:40:15 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line with put function
- * rerurn : 0 (sucess)
+ * main - Entry point
+ *
+ * Description - A program that prints a line with put function
+ *
+ * Rerurn: 0 (sucess)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
