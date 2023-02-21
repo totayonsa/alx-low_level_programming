7268802b7d338d496ad406902ea2f600e7b5c06d
totayonsa <dankipesa@gmail.com>
2023-02-20 16:31:13 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unisd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * return 1 (success0
+ */
+int main(void)
+{
+fput("and that piec of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+}
